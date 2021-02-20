852d83f1b7ef9ef1de4665fa5ff0fed4cd1521f4
scavchik <scavchik@noemail.ru>
2021-02-20 12:45:06 +0100

    Download queue context menu

--- src/main/java/com/github/engatec/vdl/controller/QueueController.java
+++ src/main/java/com/github/engatec/vdl/controller/QueueController.java
@@ -1,20 +1,29 @@
 package com.github.engatec.vdl.controller;
 
 import java.nio.file.Path;
+import java.util.ResourceBundle;
 
+import com.github.engatec.vdl.core.ApplicationContext;
 import com.github.engatec.vdl.core.QueueManager;
 import com.github.engatec.vdl.core.action.DownloadQueueItemAction;
 import com.github.engatec.vdl.model.DownloadStatus;
 import com.github.engatec.vdl.model.QueueItem;
+import javafx.beans.binding.Bindings;
+import javafx.beans.property.ObjectProperty;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.ContextMenu;
+import javafx.scene.control.Label;
+import javafx.scene.control.MenuItem;
 import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableRow;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.ProgressBarTableCell;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.stage.Stage;
+import org.apache.commons.lang3.NotImplementedException;
 
 public class QueueController extends StageAwareController {
 
@@ -41,6 +50,8 @@ public class QueueController extends StageAwareController {
 
     @FXML
     public void initialize() {
+        downloadQueueTableView.setPlaceholder(new Label(ApplicationContext.INSTANCE.getResourceBundle().getString("stage.queue.table.placeholder")));
+
         statusTableColumn.setCellValueFactory(new PropertyValueFactory<>("status"));
         urlTableColumn.setCellValueFactory(new PropertyValueFactory<>("url"));
         sizeTableColumn.setCellValueFactory(new PropertyValueFactory<>("size"));
@@ -54,6 +65,66 @@ public class QueueController extends StageAwareController {
         startDownloadBtn.setOnAction(this::handleStartDownloadButtonClick);
         cleanupBtn.setOnAction(this::handleCleanupButtonClick);
         closeBtn.setOnAction(this::handleCloseButtonClick);
+
+        downloadQueueTableView.setRowFactory(tableView -> {
+            TableRow<QueueItem> row = new TableRow<>();
+            ContextMenu contextMenu = createContextMenu(row);
+            row.contextMenuProperty().bind(
+                    Bindings.when(row.emptyProperty().not())
+                            .then(contextMenu)
+                            .otherwise((ContextMenu) null)
+            );
+            return row;
+        });
+    }
+
+    private ContextMenu createContextMenu(TableRow<QueueItem> row) {
+        ResourceBundle resourceBundle = ApplicationContext.INSTANCE.getResourceBundle();
+        ContextMenu ctxMenu = new ContextMenu();
+
+        MenuItem runNowMenuItem = new MenuItem(resourceBundle.getString("stage.queue.table.contextmenu.runnow"));
+        runNowMenuItem.setOnAction(e -> {
+            new DownloadQueueItemAction(row.getItem()).perform();
+            e.consume();
+        });
+
+        MenuItem cancelMenuItem = new MenuItem(resourceBundle.getString("stage.queue.table.contextmenu.cancel"));
+        cancelMenuItem.setOnAction(e -> {
+            throw new NotImplementedException();
+            // e.consume();
+        });
+
+        MenuItem resumeMenuItem = new MenuItem(resourceBundle.getString("stage.queue.table.contextmenu.resume"));
+        resumeMenuItem.setOnAction(e -> {
+            new DownloadQueueItemAction(row.getItem()).perform();
+            e.consume();
+        });
+
+        MenuItem deleteMenuItem = new MenuItem(resourceBundle.getString("stage.queue.table.contextmenu.delete"));
+        deleteMenuItem.setOnAction(e -> {
+            data.remove(row.getItem());
+            e.consume();
+        });
+
+        row.itemProperty().addListener((observable, oldValue, newValue) -> {
+            if (newValue == null) {
+                runNowMenuItem.visibleProperty().unbind();
+                cancelMenuItem.visibleProperty().unbind();
+                resumeMenuItem.visibleProperty().unbind();
+                deleteMenuItem.visibleProperty().unbind();
+                return;
+            }
+
+            DownloadStatus currentStatus = newValue.getStatus();
+            ObjectProperty<DownloadStatus> itemStatusProperty = newValue.statusProperty();
+            runNowMenuItem.visibleProperty().bind(Bindings.createBooleanBinding(() -> currentStatus == DownloadStatus.READY, itemStatusProperty));
+            cancelMenuItem.visibleProperty().bind(Bindings.createBooleanBinding(() -> currentStatus == DownloadStatus.IN_PROGRESS, itemStatusProperty));
+            resumeMenuItem.visibleProperty().bind(Bindings.createBooleanBinding(() -> currentStatus == DownloadStatus.CANCELLED, itemStatusProperty));
+            deleteMenuItem.visibleProperty().bind(Bindings.createBooleanBinding(() -> currentStatus != DownloadStatus.IN_PROGRESS, itemStatusProperty));
+        });
+
+        ctxMenu.getItems().addAll(runNowMenuItem, deleteMenuItem);
+        return ctxMenu;
     }
 
     private void handleStartDownloadButtonClick(ActionEvent event) {
@@ -62,10 +133,12 @@ public class QueueController extends StageAwareController {
                 new DownloadQueueItemAction(item).perform();
             }
         }
+        event.consume();
     }
 
     private void handleCleanupButtonClick(ActionEvent event) {
         data.removeIf(item -> item.getStatus() == DownloadStatus.FINISHED);
+        event.consume();
     }
 
     private void handleCloseButtonClick(ActionEvent event) {
--- src/main/java/com/github/engatec/vdl/model/DownloadStatus.java
+++ src/main/java/com/github/engatec/vdl/model/DownloadStatus.java
@@ -1,25 +1,28 @@
 package com.github.engatec.vdl.model;
 
+import com.github.engatec.vdl.core.ApplicationContext;
+
 public enum DownloadStatus {
 
-    READY("Ready"),
-    IN_PROGRESS("In progress"),
-    FAILED("Failed"),
-    FINISHED("Finished");
+    READY("stage.queue.status.ready"),
+    IN_PROGRESS("stage.queue.status.inprogress"),
+    CANCELLED("stage.queue.status.cancelled"),
+    FAILED("stage.queue.status.failed"),
+    FINISHED("stage.queue.status.finished");
 
-    private final String uiValue;
+    private final String msgKey;
 
-    DownloadStatus(String uiValue) {
-        this.uiValue = uiValue;
+    DownloadStatus(String msgKey) {
+        this.msgKey = msgKey;
     }
 
-    public String getUiValue() {
-        return uiValue;
+    public String getMsgKey() {
+        return msgKey;
     }
 
 
     @Override
     public String toString() {
-        return uiValue;
+        return ApplicationContext.INSTANCE.getResourceBundle().getString(msgKey);
     }
 }
