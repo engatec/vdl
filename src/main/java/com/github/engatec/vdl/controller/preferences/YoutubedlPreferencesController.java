aecba84833c3ae504e13fbf65e563142bfc15509
scavchik <scavchik@noemail.ru>
2021-03-02 20:45:15 +0100

    A tooltip for configuration file checkbox

--- src/main/java/com/github/engatec/vdl/controller/preferences/YoutubedlPreferencesController.java
+++ src/main/java/com/github/engatec/vdl/controller/preferences/YoutubedlPreferencesController.java
@@ -3,15 +3,22 @@ package com.github.engatec.vdl.controller.preferences;
 import java.io.File;
 
 import com.github.engatec.vdl.controller.StageAwareController;
+import com.github.engatec.vdl.core.ApplicationContext;
 import com.github.engatec.vdl.core.preferences.propertyholder.YoutubedlPropertyHolder;
+import com.github.engatec.vdl.ui.Icons;
+import com.github.engatec.vdl.util.Svg;
 import javafx.beans.binding.BooleanBinding;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.Group;
 import javafx.scene.control.Button;
 import javafx.scene.control.CheckBox;
+import javafx.scene.control.ContentDisplay;
 import javafx.scene.control.TextField;
+import javafx.scene.control.Tooltip;
 import javafx.stage.FileChooser;
 import javafx.stage.Stage;
+import javafx.util.Duration;
 
 public class YoutubedlPreferencesController extends StageAwareController {
 
@@ -45,6 +52,15 @@ public class YoutubedlPreferencesController extends StageAwareController {
     }
 
     private void initConfigFileSettings() {
+        Group infoIcon = Icons.info();
+        Svg.scale(infoIcon, 0.8);
+        Tooltip tooltip = new Tooltip(ApplicationContext.INSTANCE.getResourceBundle().getString("preferences.youtubedl.checkbox.configitem.tooltip"));
+        tooltip.setShowDelay(Duration.ZERO);
+        tooltip.setShowDuration(Duration.INDEFINITE);
+        Tooltip.install(infoIcon, tooltip);
+        useConfigFileCheckBox.setGraphic(infoIcon);
+        useConfigFileCheckBox.setContentDisplay(ContentDisplay.RIGHT);
+
         BooleanBinding configFileCheckBoxUnselected = useConfigFileCheckBox.selectedProperty().not();
         configFileTextField.disableProperty().bind(configFileCheckBoxUnselected);
         configFileChooseBtn.disableProperty().bind(configFileCheckBoxUnselected);
