a21870f471706b3ee6d38a9659fb9e8ea2e8d00e
scavchik <scavchik@noemail.ru>
2021-11-06 10:36:43 +0100

    Refactoring. Using public list implementation rather than internal one.

--- src/main/java/com/github/engatec/vdl/ui/CheckBoxGroup.java
+++ src/main/java/com/github/engatec/vdl/ui/CheckBoxGroup.java
@@ -3,9 +3,9 @@ package com.github.engatec.vdl.ui;
 import java.util.List;
 import java.util.function.Consumer;
 
-import com.sun.javafx.collections.TrackableObservableList;
 import javafx.beans.value.ChangeListener;
-import javafx.collections.ListChangeListener.Change;
+import javafx.collections.FXCollections;
+import javafx.collections.ListChangeListener;
 import javafx.collections.ObservableList;
 import javafx.scene.control.CheckBox;
 
@@ -14,18 +14,7 @@ public class CheckBoxGroup {
     private final CheckBox root;
     private final ChangeListener<Boolean> rootSelectedChangeListener;
     private final ChangeListener<Boolean> childSelectedChangeListener;
-    private final ObservableList<CheckBox> checkBoxes = new TrackableObservableList<>() {
-        @Override
-        protected void onChanged(Change<CheckBox> change) {
-            while (change.next()) {
-                for (CheckBox checkBox : change.getRemoved()) {
-                    checkBox.selectedProperty().removeListener(childSelectedChangeListener);
-                }
-
-                updateSelection(change.getList());
-            }
-        }
-    };
+    private final ObservableList<CheckBox> checkBoxes = FXCollections.observableArrayList();
 
     private Consumer<Long> onSelectionUpdateListener;
 
@@ -43,9 +32,19 @@ public class CheckBoxGroup {
         };
         root.selectedProperty().addListener(rootSelectedChangeListener);
         this.root = root;
+
+        checkBoxes.addListener((ListChangeListener<CheckBox>) change -> {
+            while (change.next()) {
+                for (CheckBox checkBox : change.getRemoved()) {
+                    checkBox.selectedProperty().removeListener(childSelectedChangeListener);
+                }
+
+                updateSelection(change.getList());
+            }
+        });
     }
 
-    private void updateSelection(List<CheckBox> list) {
+    private void updateSelection(List<? extends CheckBox> list) {
         boolean allSelected = list.stream().allMatch(CheckBox::isSelected);
         root.selectedProperty().removeListener(rootSelectedChangeListener);
         root.setSelected(allSelected);
