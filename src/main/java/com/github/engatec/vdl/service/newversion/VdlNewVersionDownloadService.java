37f8030197da80e97175e9359d0f3e18e64c29d4
scavchik <scavchik@noemail.ru>
2021-12-31 14:54:16 +0100

    Correct OS dependent program update

--- src/main/java/com/github/engatec/vdl/service/newversion/VdlNewVersionDownloadService.java
+++ src/main/java/com/github/engatec/vdl/service/newversion/VdlNewVersionDownloadService.java
@@ -7,6 +7,7 @@ import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 import java.nio.file.Path;
 import java.time.Duration;
+import java.util.StringJoiner;
 
 import com.github.engatec.vdl.core.ApplicationContext;
 import com.github.engatec.vdl.dto.github.AssetDto;
@@ -14,6 +15,8 @@ import com.github.engatec.vdl.dto.github.ReleaseDto;
 import com.github.engatec.vdl.exception.AppException;
 import javafx.concurrent.Service;
 import javafx.concurrent.Task;
+import org.apache.commons.lang3.StringUtils;
+import org.apache.commons.lang3.SystemUtils;
 
 public class VdlNewVersionDownloadService extends Service<Void> {
 
@@ -28,13 +31,25 @@ public class VdlNewVersionDownloadService extends Service<Void> {
         return new Task<>() {
             @Override
             protected Void call() throws IOException, InterruptedException {
-                String assetName = "vdl.jar";
+                StringJoiner assetNameStringJoiner = new StringJoiner("_", StringUtils.EMPTY, ".jar").add("vdl");
+                if (SystemUtils.IS_OS_WINDOWS) {
+                    assetNameStringJoiner.add("windows");
+                } else if (SystemUtils.IS_OS_LINUX) {
+                    assetNameStringJoiner.add("linux");
+                } else if (SystemUtils.IS_OS_MAC) {
+                    assetNameStringJoiner.add("mac");
+                } else {
+                    throw new AppException(String.format("Unknown OS '%s'", SystemUtils.OS_NAME));
+                }
+
+                String assetName = assetNameStringJoiner.toString();
+
                 String url = releaseInfo.assets().stream()
                         .filter(it -> assetName.equals(it.name()))
                         .map(AssetDto::downloadUrl)
                         .findFirst()
                         .orElseThrow(() -> new AppException(String.format("No %s asset has been found", assetName)));
-                Path downloadPath = ApplicationContext.getInstance().getAppBinariesDir().resolve(assetName + ".new");
+                Path downloadPath = ApplicationContext.getInstance().getAppBinariesDir().resolve("vdl.jar.new");
 
                 HttpClient client = HttpClient.newBuilder()
                         .version(HttpClient.Version.HTTP_1_1)
