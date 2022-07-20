0a84131b2d8cdc71c06cbe11a810305db1892b35
scavchik <scavchik@noemail.ru>
2022-07-20 16:38:26 -0400

    Removed exception throw as it works only for a single video and can prevent fetching multiple items

--- src/main/java/com/github/engatec/vdl/core/YoutubeDlManager.java
+++ src/main/java/com/github/engatec/vdl/core/YoutubeDlManager.java
@@ -61,7 +61,6 @@ public class YoutubeDlManager {
             LOGGER.warn(it);
             if (CollectionUtils.isEmpty(videoInfoList)) {
                 process.destroy();
-                throw new ProcessException(it);
             }
         });
 
