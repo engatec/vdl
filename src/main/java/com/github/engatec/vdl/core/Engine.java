ca337fcc4112b0bb49c543fa4eb7f2e2d583191b
scavchik <scavchik@noemail.ru>
2023-09-10 08:02:22 -0400

    Removed obsolete code

--- src/main/java/com/github/engatec/vdl/core/Engine.java
+++ src/main/java/com/github/engatec/vdl/core/Engine.java
@@ -1,13 +1,11 @@
 package com.github.engatec.vdl.core;
 
-import java.util.NoSuchElementException;
-
 import org.apache.commons.lang3.StringUtils;
 import org.apache.commons.lang3.SystemUtils;
 
 public enum Engine {
 
-    YT_DLP(2, "yt-dlp") {
+    YT_DLP {
         @Override
         public String resolveFileName() {
             return StringUtils.defaultIfBlank(
@@ -17,41 +15,5 @@ public enum Engine {
         }
     };
 
-    private final int configValue;
-    private final String displayValue;
-
-    Engine(int configValue, String displayValue) {
-        this.configValue = configValue;
-        this.displayValue = displayValue;
-    }
-
     public abstract String resolveFileName();
-
-    public int getConfigValue() {
-        return configValue;
-    }
-
-    public String getDisplayValue() {
-        return displayValue;
-    }
-
-    public static Engine getByConfigValue(int value) {
-        for (Engine item : values()) {
-            if (item.configValue == value) {
-                return item;
-            }
-        }
-
-        throw new NoSuchElementException("No config value '" + value + "' exists");
-    }
-
-    public static Engine getByDisplaValue(String value) {
-        for (Engine item : values()) {
-            if (item.displayValue.equals(value)) {
-                return item;
-            }
-        }
-
-        throw new NoSuchElementException("No display value '" + value + "' exists");
-    }
 }
